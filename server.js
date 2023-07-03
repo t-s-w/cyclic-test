f2d9b51edd7f8d9ee38d1713ba4a9b0f4e584509
t-s-w <tobby.satyarama@gmail.com>
2023-07-03 09:42:17 +0800

    add untracked files

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+// import favicon from 'serve-favicon';
+import logger from 'morgan';
+import { fileURLToPath } from 'url';
+import cors from 'cors'
+
+// Always require and configure near the top
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'dist' folder
+// app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, "dist")));
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+    console.log(`Express app running on port ${port}`)
+});
